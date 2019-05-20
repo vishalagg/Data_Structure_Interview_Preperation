af439efdc02eccb04a7a49ac85d37e47154a2bf3
vishalagg <vishalagg1994@gmail.com>
2019-05-20 12:40:15 +0530

    medium

---
+++ LinkedList/LL1.java
@@ -0,0 +1,78 @@
+package LinkedList;
+//Algorithm to find the nth node from end of the linked list
+
+import java.io.ObjectInputStream.GetField;
+import java.util.LinkedList;
+
+public class LL1 {
+	
+	Node head;
+	class Node {
+		int data;
+		Node next;
+		
+		Node(int data) {
+			this.data = data;
+			this.next = null;
+		}
+	}
+	
+	LL1() {
+		this.head = null;
+	}
+	
+	
+	
+	public void add(int data) {
+		Node node = new Node(data);
+		node.next = head;
+		head = node;
+	}
+	
+	public void display() {
+		Node temp = head;
+		 while (temp!=null) {
+			 System.out.print(temp.data + " ");
+			 temp = temp.next;
+		 }
+	}
+	
+	public int getKthElementFromLast(int k) {
+		int ans;
+		if (head!=null) {
+			Node temp = head;
+			Node temp2 = head;
+			while (k>0) {
+				k -= 1;
+				temp = temp.next;
+			}
+			
+			while (temp!=null) {
+				temp = temp.next;
+				temp2 = temp2.next;				
+			}
+			ans = temp2.data;
+		} else {
+			ans = -1;
+		}
+		
+		return ans;
+	}
+
+	public static void main(String[] args) {
+		
+		LL1 list = new LL1();
+		
+//		1->2->3->4->5->6
+		list.add(6);
+		list.add(5);
+		list.add(4);
+		list.add(3);
+		list.add(2);
+		list.add(1);		
+		list.display();
+		System.out.println();
+		int k = 2; //find kth element from last
+		System.out.println(k + " element from last is : "+list.getKthElementFromLast(k));
+	}
+}
